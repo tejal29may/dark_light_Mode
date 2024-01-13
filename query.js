67abf309927e57744b65803bd08114f3caf96e31
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-01-13 14:30:08 +0530

    dark light mode

---
+++ query.js
@@ -0,0 +1,10 @@
+let sun=document.getElementById("sun");
+let div=document.querySelector(".toggle");
+div.addEventListener('click', ()=>{
+// let h1=document.getElementById("head");
+// document.body.style.backgroundColor="black";
+// h1.style.color="white";
+
+var element = document.body;
+   element.classList.toggle("dark-mode");
+});
