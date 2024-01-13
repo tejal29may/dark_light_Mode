3a40d6b81f4deddfdd94da1657cc52c09dbbb1b5
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-01-13 20:15:40 +0530

    ...

--- query.js
+++ query.js
@@ -1,10 +1,27 @@
 let sun=document.getElementById("sun");
 let div=document.querySelector(".toggle");
-div.addEventListener('click', ()=>{
-// let h1=document.getElementById("head");
-// document.body.style.backgroundColor="black";
-// h1.style.color="white";
+let circle=document.getElementById("circle");
+let body=document.querySelector("body");
+let h1=document.getElementById("head");
 
-var element = document.body;
-   element.classList.toggle("dark-mode");
+body.style.backgroundColor="white";
+div.addEventListener('click', ()=>{
+if(body.style.backgroundColor==="white"){
+    body.style.backgroundColor="black";
+    h1.style.color="white";
+
+    circle.style.left="53%";
+    circle.style.transitionDuration="0.5s";
+}else{
+    body.style.backgroundColor="white";
+    circle.style.left="4%"
+    circle.style.transitionDuration="0.5s";
+    circle.style.backgroundColor="green";
+    h1.style.color="black";
+
+}
+
+// var element = document.body;
+//    element.classList.toggle("dark-mode");
+
 });
